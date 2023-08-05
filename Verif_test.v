9ef6e8815e6d6b33fd8d4b5cc085145ca59f3339
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2023-08-05 16:34:56 +0200

    Also verify main

--- Verif_test.v
+++ Verif_test.v
@@ -20,10 +20,25 @@ DECLARE _get_counter
     RETURN (Vint (Int.repr (c + 1)))
     SEP (data_at Ews tint (Vint (Int.repr (c + 1))) (gv _COUNTER)).
 
+Definition main_spec :=
+ DECLARE _main
+  WITH gv : globals
+  PRE [] main_pre prog tt gv
+  POST [ tint ]
+     PROP()
+     RETURN (Vint (Int.repr 0))
+     SEP(TT).
 
-Definition Gprog := [get_counter_spec].
+
+Definition Gprog := [get_counter_spec; main_spec].
 
 Lemma get_counter_proof: semax_body Vprog Gprog f_get_counter get_counter_spec.
+Proof.
+  start_function.
+  repeat forward.
+Qed.
+
+Lemma body_main: semax_body Vprog Gprog f_main main_spec.
 Proof.
   start_function.
   repeat forward.
