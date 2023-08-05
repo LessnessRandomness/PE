cd73a0cc0d3f08cac9a307f0b84b068fb42d49c2
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2023-08-05 16:35:14 +0200

    Old-style specs

--- Verif_test.v
+++ Verif_test.v
@@ -43,3 +43,45 @@ Proof.
   start_function.
   repeat forward.
 Qed.
+
+Require VST.floyd.Funspec_old_Notation.
+
+Module old_version.
+(* Using [Funspec_old_Notation] *)
+  Import VST.floyd.Funspec_old_Notation.
+
+  Definition get_counter_spec:=
+  DECLARE _get_counter
+    WITH gv: globals, c: Z
+    PRE []
+      PROP (Int.min_signed <= c /\ c + 1 <= Int.max_signed)
+      LOCAL (gvars gv)
+      SEP (data_at Ews tint (Vint (Int.repr c)) (gv _COUNTER))
+    POST [ tint ]
+      PROP ()
+      RETURN (Vint (Int.repr (c + 1)))
+      SEP (data_at Ews tint (Vint (Int.repr (c + 1))) (gv _COUNTER)).
+
+
+  Definition main_spec :=
+  DECLARE _main
+    WITH gv : globals
+    PRE [] main_pre prog tt gv
+    POST [ tint ]
+      PROP()
+      RETURN (Vint (Int.repr 0))
+      SEP(TT).
+  Definition Gprog := [get_counter_spec; main_spec].
+
+Lemma get_counter_proof: semax_body Vprog Gprog f_get_counter get_counter_spec.
+Proof.
+  start_function.
+  repeat forward.
+Qed.
+
+Lemma body_main: semax_body Vprog Gprog f_main main_spec.
+Proof.
+  start_function.
+  repeat forward.
+Qed.
+End old_version.
