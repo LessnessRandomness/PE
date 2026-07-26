ddb23c055ca174f11a59aeac7f42b1037231ce59
LessnessRandomness <56280933+LessnessRandomness@users.noreply.github.com>
2026-07-26 19:46:34 +0300

    Starting with the 4th problem

---
+++ EulerProject4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+bool is_palindrome(uint32_t n) {
+    if (n / 100000 != n % 10) {
+        return false;
+    }
+    if (n / 10000 % 10 != n / 10 % 10) {
+        return false;
+    }
+    if (n / 1000 % 10 != n / 100 % 10) {
+        return false;
+    }
+    return true;
+}
+
+int find() {
+    uint32_t max_value = 100000;
+    for (uint16_t x = 990; max_value < x * 999; x -= 11) {
+        for (uint32_t n = x * 999; max_value < n; n -= x) {
+            if (is_palindrome(n)) {
+                max_value = n;
+            }
+        }
+    }
+    return max_value;
+}
+
+int main() {
+    return find();
+}
+
